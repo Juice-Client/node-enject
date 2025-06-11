bab63782032b6bc0e89b373e4048d42a626f7d7f
rob <roby.pakalns@gmail.com>
2025-06-11 15:11:31 +0300

    remove artifacts folder; combine workflow jobs into one

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const path = require("path")
 
 const binaryName = "node-enject.node"
-const binaryPath = path.join(__dirname, "artifacts", binaryName)
+const binaryPath = path.join(__dirname, binaryName)
 
 module.exports = require(binaryPath)
